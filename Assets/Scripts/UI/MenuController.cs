5aacb7696433b397ef758ea1cb8a480241f58b8f
Ian Percy <ianedwardpercy@gmail.com>
2022-09-24 00:16:10 -0500

    DoTween and Menu Scripts

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DoTween;
+
+public class MenuController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        
+    }
+}
