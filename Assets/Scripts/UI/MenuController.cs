bc74c89648e4054d805ad0f11dfd0f60677c8398
Brandon <bmc939@gmail.com>
2022-09-24 00:28:58 -0500

    Reset Plugins (DoTween)

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using DoTween;
+using DG.Tweening;
 
 public class MenuController : MonoBehaviour
 {
