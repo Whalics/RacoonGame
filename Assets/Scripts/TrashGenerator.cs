bf6de84e7e874e47000f8df55d08879552ef1dfc
Brandon <bmc939@gmail.com>
2022-09-24 15:06:55 -0500

    Update Trash Generator to be more consistent

--- Assets/Scripts/TrashGenerator.cs
+++ Assets/Scripts/TrashGenerator.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TrashGenerator : MonoBehaviour
 {
-    [SerializeField] private float _x;
-    [SerializeField] private float _y;
+    [SerializeField] private float _xDist;
+    [SerializeField] private int _xAmount;
+    [SerializeField] private float _yDist;
+    [SerializeField] private int _yAmount;
+    [SerializeField] private float _random;
     [SerializeField] private int _trashAmount;
     [SerializeField] private List<Sprite> _objects;
 
@@ -13,7 +18,12 @@ public class TrashGenerator : MonoBehaviour
     {
         Generate();
     }
-    
+
+    private void OnValidate()
+    {
+        _trashAmount = _xAmount * _yAmount;
+    }
+
     [Button(Mode = ButtonMode.InPlayMode)]
     private void Generate()
     {
@@ -22,30 +32,34 @@ public class TrashGenerator : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        for (int i = 0; i < _trashAmount; i++)
+        var p = transform.position;
+        for (float x = 0; x <= _xAmount; x ++)
         {
-            var obj = new GameObject("Trash", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
-            obj.sprite = _objects[Random.Range(0, _objects.Count - 1)];
-            obj.transform.position = GetRandomPosition();
-            obj.transform.parent = transform;
+            float xPos = p.x - _xDist + x * 2 * _xDist / _xAmount;
+            for (float y = 0; y <= _yAmount; y++)
+            {
+                float yPos = p.y - _yDist + y * 2 * _yDist / _yAmount;
+                var pos = new Vector3(xPos, yPos, p.z) + _random * (Vector3)Random.insideUnitCircle;
+                CreateNewTrash(pos);
+            }
         }
     }
 
-    private Vector3 GetRandomPosition()
+    private void CreateNewTrash(Vector3 pos)
     {
-        var p = transform.position;
-        float x = p.x + Random.Range(-_x, _x);
-        float y = p.y + Random.Range(-_y, _y);
-        return new Vector3(x, y, p.z);
+        var obj = new GameObject("Trash", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
+        obj.sprite = _objects[Random.Range(0, _objects.Count - 1)];
+        obj.transform.parent = transform;
+        obj.transform.position = pos;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         var p = transform.position;
-        Gizmos.DrawLine(new Vector3(p.x - _x, p.y - _y, p.z), new Vector3(p.x + _x, p.y - _y, p.z));
-        Gizmos.DrawLine(new Vector3(p.x - _x, p.y + _y, p.z), new Vector3(p.x + _x, p.y + _y, p.z));
-        Gizmos.DrawLine(new Vector3(p.x - _x, p.y - _y, p.z), new Vector3(p.x - _x, p.y + _y, p.z));
-        Gizmos.DrawLine(new Vector3(p.x + _x, p.y - _y, p.z), new Vector3(p.x + _x, p.y + _y, p.z));
+        Gizmos.DrawLine(new Vector3(p.x - _xDist, p.y - _yDist, p.z), new Vector3(p.x + _xDist, p.y - _yDist, p.z));
+        Gizmos.DrawLine(new Vector3(p.x - _xDist, p.y + _yDist, p.z), new Vector3(p.x + _xDist, p.y + _yDist, p.z));
+        Gizmos.DrawLine(new Vector3(p.x - _xDist, p.y - _yDist, p.z), new Vector3(p.x - _xDist, p.y + _yDist, p.z));
+        Gizmos.DrawLine(new Vector3(p.x + _xDist, p.y - _yDist, p.z), new Vector3(p.x + _xDist, p.y + _yDist, p.z));
     }
 }
