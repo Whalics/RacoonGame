22bb199e4641f37b8f72876af0ced4515fffbd71
Brandon <bmc939@gmail.com>
2022-09-24 02:13:41 -0500

    Import Coffey Utils and Setup Trash Generation

--- Assets/Scripts/TrashGenerator.cs
+++ Assets/Scripts/TrashGenerator.cs
@@ -6,13 +6,36 @@ public class TrashGenerator : MonoBehaviour
 {
     [SerializeField] private float _x;
     [SerializeField] private float _y;
+    [SerializeField] private int _trashAmount;
     [SerializeField] private List<Sprite> _objects;
+
+    private void Start()
+    {
+        Generate();
+    }
+    
+    [Button(Mode = ButtonMode.InPlayMode)]
+    private void Generate()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < _trashAmount; i++)
+        {
+            var obj = new GameObject("Trash", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
+            obj.sprite = _objects[Random.Range(0, _objects.Count - 1)];
+            obj.transform.position = GetRandomPosition();
+            obj.transform.parent = transform;
+        }
+    }
 
     private Vector3 GetRandomPosition()
     {
         var p = transform.position;
-        var x = p.x + Random.Range(-_x, _x);
-        var y = p.y + Random.Range(-_y, _y);
+        float x = p.x + Random.Range(-_x, _x);
+        float y = p.y + Random.Range(-_y, _y);
         return new Vector3(x, y, p.z);
     }
 
